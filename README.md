9225913149ace6ed5f9a8c8f1ed82a9186647aec
Anton Gunov <anton@gunov.name>
2017-08-10 10:09:00 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Nginx Boilerplate
+
+Nginx boilerplate for integrating with Node.js as a reverse proxy server.
+
+## License
+  
+The code is available under the [MIT License](LICENSE).
---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+/**
+ * Load environment variables
+ */
+
+require('dotenv').config();
+
+/**
+ * Sub-tasks
+ */
+
+require('./gulp/tasks/nginx');
+
+/**
+ * Main tasks
+ */
+
+// require('./gulp/tasks/default');
