47c0c2924df7ab0a4286d1069fd50e2075250d0d
Anton Gunov <anton@gunov.name>
2017-08-30 17:06:23 +0700

    Support for preflight request

---
+++ tests/nginx.js
@@ -0,0 +1,20 @@
+const supertest = require('supertest');
+
+const NGINX_PORT = parseInt(process.env.NGINX_PORT, 10);
+
+describe('Nginx', () => {
+  const request = supertest(`http://${process.env.NGINX_FQDN}:${NGINX_PORT}`);
+
+  it('Support for preflight request', (done) => {
+    request
+      .options('/api')
+      // preflight response headers
+      .expect('Access-Control-Allow-Origin', '*')
+      .expect('Access-Control-Allow-Methods', /GET/)
+      .expect('Access-Control-Allow-Headers', /Content-Type/)
+      // no content
+      .expect('Content-Length', '0')
+      .expect(204)
+      .end(done);
+  });
+});
